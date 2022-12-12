fc1eecde09d1622c06425c561bc957e1d3a4fd5e
Linloir <3145078758@qq.com>
2022-12-12 15:45:34 +0800

    Update Camera API

--- FinalProject/camera.cpp
+++ FinalProject/camera.cpp
@@ -1,1 +1,5 @@
 #pragma once
+
+#include <GLM/ext/matrix_transform.hpp>
+
+#include "camera.h"
--- FinalProject/camera.h
+++ FinalProject/camera.h
@@ -4,17 +4,26 @@
 
 class Camera {
 private:
+    // Camera state
     glm::vec3 _position;
-    glm::vec3 _direction;
     glm::vec3 _right;
+    glm::vec3 _front;
     glm::vec3 _up;
-    float _zoom;
     float _yawRad;
     float _pitchRad;
     float _rollRad;
+    float _zoom;
 
+//private:
+//    // Camera settings
+//    float _moveStep;
+//    float _yawStep;
+//    float _pitchStep;
+//    float _rollStep;
+    
 private:
-    glm::vec3 frontVec();
+    // World settings
+    glm::vec3 _worldUp;
     
 public:
     Camera();
@@ -23,7 +32,7 @@ public:
     ~Camera();
 
 public:
-    glm::mat4 viewMatrix();
+    inline glm::mat4 viewMatrix();
     
 public:
     void move(glm::vec3 deltaVec);
