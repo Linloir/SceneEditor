984c53340c758d4bb116404a2c883fb0c2f245fb
Linloir <3145078758@qq.com>
2022-12-14 08:41:47 +0800

    UI update: frameless window - window skeleton - multi screen resize support - auto maximize

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowFlag(Qt::FramelessWindowHint);
+    w.setAttribute(Qt::WA_TranslucentBackground);
+    w.setMouseTracking(true);
     w.show();
     return a.exec();
 }
