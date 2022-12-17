d818ccd4692a8bbf3c2f35b310a93c9dea88cf73
Linloir <3145078758@qq.com>
2022-12-17 11:41:55 +0800

    [UI][ADD] Model thumbnail widget

---
+++ FinalProject/modelthumbnailwidget.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <QtOpenGLWidgets/qopenglwidget.h>
+#include <qopenglfunctions.h>
+#include <qevent.h>
+
+#include "model.h"
+#include "camera.h"
+#include "shader.h"
+#include "renderable.h"
+
+class ModelThumbnailWidget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    ModelThumbnailWidget(Model* model, QWidget* parent = 0);
+    ~ModelThumbnailWidget();
+
+private:
+    Model* _model = nullptr;
+    Renderable _object = Renderable::empty();
+    ShaderProgram _shaderProgram = ShaderProgram::empty();
+    Camera _camera;
+    
+protected:
+    virtual void initializeGL() override;
+    virtual void paintGL() override;
+    virtual void resizeGL(int w, int h) override;
+};
