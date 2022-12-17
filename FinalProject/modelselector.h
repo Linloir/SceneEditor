edeaa7f3c651971657649dc8b51363fd493b5bb2
Linloir <3145078758@qq.com>
2022-12-17 11:42:32 +0800

    [UI][ADD] Model selection list

--- FinalProject/modelselector.h
+++ FinalProject/modelselector.h
@@ -1,12 +1,40 @@
 #pragma once
 
 #include <qwidget.h>
+#include <qboxlayout.h>
+#include <qlist.h>
 
-//class ModelSelector : public QWidget
-//{
-//    Q_OBJECT
-//        
-//public:
-//    ModelSelector(QWidget* parent = 0);
-//    ~ModelSelector();
-//};
+#include "scrolllistwidget.h"
+#include "pushbutton.h"
+#include "modelselectable.h"
+#include "model.h"
+
+class ModelSelector : public QWidget
+{
+    Q_OBJECT
+        
+public:
+    ModelSelector(QWidget* parent = 0);
+    ~ModelSelector();
+
+private:
+    // UI elements
+    QVBoxLayout* _stretchLayout = nullptr;
+    PushButton* _addNewObjectBtn = nullptr;
+    ScrollListWidget* _objectList = nullptr;
+
+    // Object selection storage
+    QList<ModelSelectable*> _objectSelectables;
+
+private:
+    // Slot functions
+    void addNewObject();
+    
+    // Util functions
+    QString getNewObjectPath();
+    Model* loadObject(const QString& path);
+    void removeObject(ModelSelectable* selectable);
+
+signals:
+    void onObjectSelected(Model* selectedModel);
+};
