fe571568aec9ca9828f3ae114243403b4ff6b80b
Linloir <3145078758@qq.com>
2022-12-19 17:11:15 +0800

    [CORE][FIX] Fix fragment shader - wrong uniform struct member name causing failure in setting spot light

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -11,6 +11,10 @@ int main(int argc, char *argv[])
     
     QFontDatabase::addApplicationFont(":/fonts/font_awesome_6_regular_free.otf"); // Add font awesome font to application
     QFontDatabase::addApplicationFont(":/fonts/font_awesome_6_solid_free.otf"); // Add font awesome font to application
+    QFontDatabase::addApplicationFont(":/fonts/corbel.ttf"); // Add corbel font to application
+    QFontDatabase::addApplicationFont(":/fonts/Deng.ttf"); // Add Deng Xian font to application
+    QFontDatabase::addApplicationFont(":/fonts/Dengb.ttf"); // Add Deng Xian Bold font to application
+    QFontDatabase::addApplicationFont(":/fonts/Dengl.ttf"); // Add Deng Xian Light font to application
 
     MainWindow w;
     w.setMouseTracking(true);
